86736ccaa3817e4feeba12bf679e4729a1889e73
naotaka.miyabara <na.miya@kdk-nagano.co.jp>
2021-06-28 04:12:32 +0000

    Procfile

--- app/User.php
+++ app/User.php
@@ -137,7 +137,7 @@ class User extends Authenticatable
      */
     public function loadRelationshipCounts()
     {
-        $this->loadCount('microposts');
+        $this->loadCount(['microposts', 'followings', 'followers']);
     }
     
 }
